1f1d7a9b5b998c7b27351f9170bb86b275752909
Aazam Ben <aazamben168@gmail.com>
2022-08-09 19:46:53 +0530

    Completed BusinessList

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,3 +1,21 @@
-import React from 'react';
-import './Business.css';
-import '../Business/Business';
+import React from "react";
+import "./Business.css";
+import "../Business/Business";
+import Business from "../Business/Business";
+
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="BusinessList">
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+      </div>
+    );
+  }
+}
+
+export default BusinessList;
