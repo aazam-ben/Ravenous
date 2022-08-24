45f6c6d62b9515c7559357b1e4f75802d05c61d7
Aazam Ben <aazamben168@gmail.com>
2022-08-24 18:39:27 +0530

    Handle search of Yelp

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,13 +21,13 @@ const businesses = [business, business, business, business, business, business];
 
 class App extends React.Component {
   searchYelp(term, location, sortBy) {
-    console.log(`Searching Yelp with Pizza, Brooklyn, best_match`);
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
   }
   render() {
     return (
       <div className="App">
         <h1>Ravenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp}/>
         <BusinessList businesses={businesses} />
       </div>
     );
