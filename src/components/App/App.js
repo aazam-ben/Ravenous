a2d6189705dd1fc62c89fea81f7c96002c063f6d
Aazam Ben <aazamben168@gmail.com>
2022-08-24 18:30:03 +0530

    setup searchYelp method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -19,14 +19,19 @@ const business = {
 
 const businesses = [business, business, business, business, business, business];
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Ravenous</h1>
-      <SearchBar />
-      <BusinessList businesses={businesses} />
-    </div>
-  );
+class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with Pizza, Brooklyn, best_match`);
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Ravenous</h1>
+        <SearchBar />
+        <BusinessList businesses={businesses} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./SearchBar.css";
-import { render } from "react-dom";
 
 class SearchBar extends React.Component {
   constructor(props) {
