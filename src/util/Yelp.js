82d166f7d796b08ce9a37a9e2405192960f0ed6d
Aazam Ben <aazamben168@gmail.com>
2022-09-01 19:16:16 +0530

    add instructions to Yelp funcations.

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,2 +1,14 @@
 const apiKey =
   "OGz2xgqVAvGSgEOunyGdX26oqBXAbEEdZHm3bvLjuZnXQ5erXVYJatSxCsvC6zu0B3S_fXn2xq3Ih4D7m_tU5WX61yH298bttsiPHz08IoF81dIjU9iU1j1lR5APY3Yx";
+const yelp = {
+  search(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    );
+  },
+};
